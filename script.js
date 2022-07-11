8dd01c2f9360003afe9b83f49b0ff8de83ada105
Marcos Rodrigues <mquinhoz@hotmail.com>
2022-07-11 17:12:09 -0300

    Uso do DOM e finalização do JS

--- script.js
+++ script.js
@@ -1,96 +1,123 @@
-let item = {
-  imagem: "./assets/img/painting/clock.jpg",
-  nome: "DarkTimer",
-  valor: "R$ 105,99",
-  id: 'Action Figure'
-};
-let item0 = {
-  imagem: "./assets/img/painting/gamepad.jpg",
-  nome: "Infra Game",
-  valor: "R$ 77,88",
-  id: 'Action Figure'
-};
-let item1 = {
-  imagem: "./assets/img/painting/personagem.jpg",
-  nome: "Stin Head",
-  valor: "R$ 68,99",
-  id: 'Action Figure'
-};
-let item2 = {
-  imagem: "./assets/img/actions/animewoman.jpg",
-  nome: "Adventurer like Hell",
-  valor: "R$ 82,90",
-  id: 'Paintings'
-};
-let item3 = {
-  imagem: "./assets/img/actions/dragonballpersonagem.jpg",
-  nome: "Goku",
-  valor: "R$ 78,90",
-  id: 'Paintings'
-};
-let item4 = {
-  imagem: "./assets/img/actions/starwarspersonagem.jpg",
-  nome: "Baby Yoda",
-  valor: "R$ 82.90",
-  id: 'Paintings'
-};
- 
-let paintList = []
-let figureList = []
+let item = [
+  {
+    imagem: "./assets/img/painting/clock.jpg",
+    nome: "DarkTimer",
+    valor: "R$ 105,99",
+    id: "Paintings",
+  },
+  {
+    imagem: "./assets/img/painting/gamepad.jpg",
+    nome: "Infra Game",
+    valor: "R$ 77,88",
+    id: "Paintings",
+  },
+  {
+    imagem: "./assets/img/painting/personagem.jpg",
+    nome: "Stin Head",
+    valor: "R$ 68,99",
+    id: "Paintings",
+  }];
+let item2 = [  {
+    imagem: "./assets/img/actions/animewoman.jpg",
+    nome: "Hell Wanderer",
+    valor: "R$ 82,90",
+    id: "Action Figure",
+  },
+  {
+    imagem: "./assets/img/actions/dragonballpersonagem.jpg",
+    nome: "Goku",
+    valor: "R$ 78,90",
+    id: "Action Figure",
+  },
+  {
+    imagem: "./assets/img/actions/starwarspersonagem.jpg",
+    nome: "Baby Yoda",
+    valor: "R$ 82.90",
+    id: "Action Figure",
+  }]
+;
+
+let paintList = [];
+let figureList = [];
+
 ///////////////////////////////////////////
 
 function criarItemPintura(obj) {
-  for (let i in obj){
-  if (obj.id !== 'Paintings'){return 'Use a função para Action Figures'}
-}
+  for (let i = 0; i < obj.length; i++) {
+    if (obj[i].id !== "Paintings") {
+      return "Use a função para Action Figures";
+    } else {
+      paintList.push(obj[i]);
+    }
+  }
   delete obj.id;
-  paintList.push(obj)
-  return paintList
+  return paintList;
 }
-console.log(criarItemPintura(item2));
+criarItemPintura(item);
+
 ////////////////////////////////////////
 
 function criarItemFigura(obj) {
-    for (let i in obj){
-        if (obj.id !== 'Action Figure'){return 'Use a função para Paintings'}
-      }
-        delete obj.id;
-        figureList.push(obj)
-        return figureList
-      }
-    
-console.log(criarItemFigura(item));
+  for (let i = 0; i < obj.length; i++) {
+    if (obj[i].id !== "Action Figure") {
+      return "Use a função para Paintings";
+    } else {figureList.push(obj[i]);
+    }
+  }
+  delete obj.id;
+  return figureList;
+}
+
+criarItemFigura(item2);
 /////////////////////////////////////////
 
 let listaPinturas = document.getElementsByClassName("list")[0];
 let listaFiguras = document.getElementsByClassName("list")[1];
 
-function ActionFigures(list) {
+function paintings(list) {
   for (let i = 0; i < list.length; i++) {
-    //let itemLista = document.createElement('li');
+    let itemLista = document.createElement('li');
     let imagemProduto = document.createElement("img");
-    // console.log(imagemProduto)
     let nomeProduto = document.createElement("span");
     let valorProduto = document.createElement("span");
-    let clasList = document.querySelector(".list");
-    let clasCard = document.querySelector(".card");
-    let clasImg = document.querySelector(".image");
-    let clasItN = document.querySelector(".item-name");
-    let clasItV = document.querySelector(".item-value");
 
-   /* nomeProduto.innerText = `${list[i].nome}`;
-   // valorProduto.innerText = `${list[i].valor}`;
-    //imagemProduto.innerText = `${list[i].imagem}`;
+    itemLista.classList.add("card");
+    imagemProduto.classList.add("image");
+    nomeProduto.classList.add("item-name");
+    valorProduto.classList.add("item-value");
 
-    figProduct.appendChild(nomeProduto);
-    figProduct.appendChild(valorProduto);
-    figProduct.appendChild(imagemProduto);
-    /*itemLista.appendChild(clasList);
-    itemLista.appendChild(clasCard);
-    itemLista.appendChild(clasImg);
-    itemLista.appendChild(clasItN);
-    itemLista.appendChild(clasItV);
-    listaFiguras.appendChild(figProduct);*/
+    nomeProduto.innerText = list[i].nome;
+    valorProduto.innerText = list[i].valor;
+    imagemProduto.src = list[i].imagem;
+
+    listaPinturas.appendChild(itemLista);
+    itemLista.appendChild(imagemProduto);
+    itemLista.appendChild(nomeProduto);
+    itemLista.appendChild(valorProduto);
   }
 }
-console.log(ActionFigures(criarItemFigura));
+paintings(paintList)
+
+function actionFigures(list) {
+  for (let i = 0; i < list.length; i++) {
+    let itemLista = document.createElement('li');
+    let imagemProduto = document.createElement("img");
+    let nomeProduto = document.createElement("span");
+    let valorProduto = document.createElement("span");
+   
+    itemLista.classList.add("card");
+    imagemProduto.classList.add("image");
+    nomeProduto.classList.add("item-name");
+    valorProduto.classList.add("item-value");
+    
+    nomeProduto.innerText = list[i].nome;
+    valorProduto.innerText = list[i].valor;
+    imagemProduto.src = list[i].imagem;
+
+    listaFiguras.appendChild(itemLista);
+    itemLista.appendChild(imagemProduto);
+    itemLista.appendChild(nomeProduto);
+    itemLista.appendChild(valorProduto);
+  }
+}
+actionFigures(figureList)
